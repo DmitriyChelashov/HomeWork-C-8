e24895ae930f3d6bfc7a0778a6a18a726c4aeae3
«Dmitriy» <arsenal_lion@mail.ru>
2022-09-02 16:34:39 +0300

    Taks made

---
+++ Tasks/Task_2/Program.cs
@@ -0,0 +1,64 @@
+﻿// В двумерном массиве целых чисел. Удалить строку и столбец, на пересечении которых расположен наименьший элемент.
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+Console.WriteLine ("Введите количество строк в массиве: ");
+int n = int.Parse (Console.ReadLine ());
+Console.WriteLine ("Введите количество столбцов в массиве: ");
+int k = int.Parse (Console.ReadLine ());
+int[,] array = new int[n, k];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int x = 0;
+int y = 0;
+int min = array[0, 0];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        if (array[i, j] < min)
+        {
+            min = array[i, j];
+            x = i;
+            y = j;
+        }
+    }
+}
+Console.WriteLine("Наименьший элемент: " + min);
+
+Console.WriteLine("Вывод преобразованной матрицы");
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        {
+            if ((i == x) || (j == y))
+                continue;
+            else
+                Console.Write(array[i, j] + " ");
+        }
+    }
+    Console.WriteLine();
+}
